e4aea7b7a64eedb273fda11f3f8c31f2f800117e
bbaktaeho <lth9036@gmail.com>
2020-07-16 15:12:51 +0900

    Test: distribute 3

--- back/models/index.js
+++ back/models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/dbConfig')[env];
+const config = require('../config/config')[env];
 const db = {};
 
 // connect mysql
